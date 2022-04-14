058ae75a682a6811f0843c4ff48b1a4249f32f80
asif <mdasifahamed89@yahoo.com>
2022-04-14 13:21:21 +0600

    Starting

---
+++ weather.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const https = require('https'); // This library required for loadung data from api
+const app = express();
+const port = 3000;
+
+app.get('/', function(req,res){
+  const url = 'https://api.openweathermap.org/data/2.5/weather?appid=43b7c5df43d7971ae8ff4a209fcdf2bc&units=metric&q=Dhaka';
+  https.get(url,function(response){ // this method helps on getting the feacthed data from the api
+    console.log(response.statusCode);
+
+
+    response.on("data",function(data){ // to response data gotten from the api.
+      const weatherData = JSON.parse(data); // parsing the the data gotten form api to json format
+      const temp = weatherData.main.temp; // storing the specific chosen data to a variable.
+      const descp = weatherData.weather[0].description // storing the specific chosen data to a variable.
+      console.log(weatherData);
+      console.log(temp);
+      console.log(descp);
+    })
+  })
+  res.send("Server is running")
+});
+
+
+app.listen(port,function(){
+  console.log("Server Started");
+});
